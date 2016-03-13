2511754f39096e2acdcd06cd00534106a854d1db
Petri Kainulainen <petri.kainulainen@gmail.com>
2016-03-13 16:27:33 +0200

    Move the persistence configuration into a separate class

--- spring/src/main/java/net/petrikainulainen/springbatch/SpringBatchExampleContext.java
+++ spring/src/main/java/net/petrikainulainen/springbatch/SpringBatchExampleContext.java
@@ -1,8 +1,5 @@
 package net.petrikainulainen.springbatch;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,18 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import java.util.Properties;
 
 /**
  * @author Petri Kainulainen
@@ -29,102 +16,10 @@ import java.util.Properties;
 @Configuration
 @ComponentScan
 @EnableBatchProcessing
-@EnableJpaRepositories(basePackages = {
-        "net.petrikainulainen.springbatch"
-})
 @EnableScheduling
-@EnableTransactionManagement
-@Import({WebAppContext.class})
+@Import({PersistenceContext.class, WebAppContext.class})
 @PropertySource("classpath:application.properties")
 public class SpringBatchExampleContext {
-
-    private static final String[] ENTITY_PACKAGES = {
-            "net.petrikainulainen.springbatch"
-    };
-
-
-    private static final String PROPERTY_NAME_DB_DRIVER_CLASS = "db.driver";
-    private static final String PROPERTY_NAME_DB_PASSWORD = "db.password";
-    private static final String PROPERTY_NAME_DB_URL = "db.url";
-    private static final String PROPERTY_NAME_DB_USER = "db.username";
-
-    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
-    private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
-    private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
-    private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY = "hibernate.ejb.naming_strategy";
-    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
-
-    private static final String PROPERTY_NAME_LIQUIBASE_CHANGE_LOG = "liquibase.change-log";
-
-    /**
-     * Creates and configures the HikariCP datasource bean.
-     * @param env   The runtime environment of  our application.
-     * @return
-     */
-    @Bean(destroyMethod = "close")
-    DataSource dataSource(Environment env) {
-        HikariConfig dataSourceConfig = new HikariConfig();
-
-        dataSourceConfig.setDriverClassName(env.getRequiredProperty(PROPERTY_NAME_DB_DRIVER_CLASS));
-        dataSourceConfig.setJdbcUrl(env.getRequiredProperty(PROPERTY_NAME_DB_URL));
-        dataSourceConfig.setUsername(env.getRequiredProperty(PROPERTY_NAME_DB_USER));
-        dataSourceConfig.setPassword(env.getRequiredProperty(PROPERTY_NAME_DB_PASSWORD));
-
-        return new HikariDataSource(dataSourceConfig);
-    }
-
-    /**
-     * Creates the bean that creates the JPA entity manager factory.
-     * @param dataSource    The datasource that provides the database connections.
-     * @param env           The runtime environment of  our application.
-     * @return
-     */
-    @Bean
-    LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, Environment env) {
-        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactoryBean.setDataSource(dataSource);
-        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        entityManagerFactoryBean.setPackagesToScan(ENTITY_PACKAGES);
-
-        Properties jpaProperties = new Properties();
-
-        //Configures the used database dialect. This allows Hibernate to create SQL
-        //that is optimized for the used database.
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_DIALECT, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
-
-        //Specifies the action that is invoked to the database when the Hibernate
-        //SessionFactory is created or closed.
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO));
-
-        //Configures the naming strategy that is used when Hibernate creates
-        //new database objects and schema elements
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY));
-
-        //If the value of this property is true, Hibernate writes all SQL
-        //statements to the console.
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
-
-        //If the value of this property is true, Hibernate will use prettyprint
-        //when it writes SQL to the console.
-        jpaProperties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, env.getRequiredProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL));
-
-        entityManagerFactoryBean.setJpaProperties(jpaProperties);
-
-        return entityManagerFactoryBean;
-    }
-
-    /**
-     * Creates the transaction manager bean that integrates the used JPA provider with the
-     * Spring transaction mechanism.
-     * @param entityManagerFactory  The used JPA entity manager factory.
-     * @return
-     */
-    @Bean
-    JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-    }
 
     @Bean
     PropertySourcesPlaceholderConfigurer propertyPlaceHolderConfigurer() {
@@ -134,13 +29,5 @@ public class SpringBatchExampleContext {
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    SpringLiquibase springLiquibase(DataSource dataSource, Environment env) {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setDataSource(dataSource);
-        liquibase.setChangeLog(env.getRequiredProperty(PROPERTY_NAME_LIQUIBASE_CHANGE_LOG));
-        return liquibase;
     }
 }
