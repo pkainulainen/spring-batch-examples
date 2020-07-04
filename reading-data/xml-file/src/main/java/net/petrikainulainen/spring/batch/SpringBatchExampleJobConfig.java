a801fe01e8dd7a3bfcb3587c23b80419bc989f14
Petri Kainulainen <petri.kainulainen@gmail.com>
2020-07-04 20:02:43 +0300

    Add example: reading information from an XML file

---
+++ reading-data/xml-file/src/main/java/net/petrikainulainen/spring/batch/SpringBatchExampleJobConfig.java
@@ -0,0 +1,75 @@
+package net.petrikainulainen.spring.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.xml.StaxEventItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+/**
+ * This configuration class configures the Spring Batch job that
+ * is used to demonstrate that our Maven and Gradle projects declare
+ * all required dependencies.
+ */
+@Configuration
+public class SpringBatchExampleJobConfig {
+
+    @Bean
+    ItemReader<StudentDTO> itemReader() {
+        StaxEventItemReader<StudentDTO> xmlFileReader = new StaxEventItemReader<>();
+        xmlFileReader.setResource(new ClassPathResource("data/students.xml"));
+        xmlFileReader.setFragmentRootElementName("student");
+
+        Jaxb2Marshaller studentMarshaller = new Jaxb2Marshaller();
+        studentMarshaller.setClassesToBeBound(StudentDTO.class);
+        xmlFileReader.setUnmarshaller(studentMarshaller);
+
+        return xmlFileReader;
+    }
+
+    @Bean
+    ItemWriter<StudentDTO> itemWriter() {
+        return new LoggingItemWriter();
+    }
+
+    /**
+     * Creates a bean that represents the only step of our batch job.
+     * @param reader
+     * @param writer
+     * @param stepBuilderFactory
+     * @return
+     */
+    @Bean
+    Step exampleJobStep(ItemReader<StudentDTO> reader,
+                        ItemWriter<StudentDTO> writer,
+                        StepBuilderFactory stepBuilderFactory) {
+        return stepBuilderFactory.get("exampleJobStep")
+                .<StudentDTO, StudentDTO>chunk(1)
+                .reader(reader)
+                .writer(writer)
+                .build();
+    }
+
+    /**
+     * Creates a bean that represents our example batch job.
+     * @param exampleJobStep
+     * @param jobBuilderFactory
+     * @return
+     */
+    @Bean
+    Job exampleJob(Step exampleJobStep,
+                   JobBuilderFactory jobBuilderFactory) {
+        return jobBuilderFactory.get("exampleJob")
+                .incrementer(new RunIdIncrementer())
+                .flow(exampleJobStep)
+                .end()
+                .build();
+    }
+}
---
+++ reading-data/xml-file/src/main/java/net/petrikainulainen/spring/batch/StudentDTO.java
@@ -0,0 +1,58 @@
+package net.petrikainulainen.spring.batch;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Contains the information that's read from the XML file.
+ *
+ * If you don't want to add public accessor methods to this class,
+ * you must annotate this class with the following annotation:
+ *
+ * {@code @XmlAccessorType(XmlAccessType.FIELD)}
+ *
+ * @see  <a href="https://stackoverflow.com/a/22196362">StackOverflow answer: What's the difference between JAXB annotations put on getter versus setters versus members?</a>
+ */
+@XmlRootElement(name="student")
+
+public class StudentDTO {
+
+    private String emailAddress;
+    private String name;
+    private String purchasedPackage;
+
+    public StudentDTO() {}
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPurchasedPackage() {
+        return purchasedPackage;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPurchasedPackage(String purchasedPackage) {
+        this.purchasedPackage = purchasedPackage;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentDTO{" +
+                "emailAddress='" + emailAddress + '\'' +
+                ", name='" + name + '\'' +
+                ", purchasedPackage='" + purchasedPackage + '\'' +
+                '}';
+    }
+}
