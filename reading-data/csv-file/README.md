2d787dc674f9453b34c8a075163056889a24b5e2
Petri Kainulainen <petri.kainulainen@gmail.com>
2020-07-09 20:18:29 +0300

    Add example: reading information from a CSV file

---
+++ reading-data/csv-file/README.md
@@ -0,0 +1,24 @@
+# Spring Batch Tutorial: Reading Data From a CSV File
+
+You can read the relevant blog post by clicking the following link:
+
+* [Spring Batch Tutorial: Reading Data From a CSV File](https://www.petrikainulainen.net/programming/spring-framework/spring-batch-tutorial-reading-information-from-a-file/)
+
+# Prerequisites
+
+You need to install the following tools if you want to run this application:
+
+* [JDK](https://adoptopenjdk.net/) (the application is tested with OpenJDK 14)
+* [Maven](http://maven.apache.org/) (the application is tested with Maven 3.6.3)
+
+# Running the Application With Maven
+
+You can run the application by using the following command:
+
+    mvn clean spring-boot:run
+
+# Running the Application With Gradle
+
+You can run the application by using the following command:
+
+    gradle clean bootRun
---
+++ reading-data/csv-file/src/main/java/net/petrikainulainen/spring/batch/SpringBatchExampleJobConfig.java
@@ -0,0 +1,104 @@
+package net.petrikainulainen.spring.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.LineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * This configuration class configures the Spring Batch job that
+ * is used to demonstrate that item reader reads the correct
+ * information from the CSV file.
+ */
+@Configuration
+public class SpringBatchExampleJobConfig {
+
+    @Bean
+    public ItemReader<StudentDTO> itemReader() {
+        FlatFileItemReader<StudentDTO> csvFileReader = new FlatFileItemReader<>();
+        csvFileReader.setResource(new ClassPathResource("data/students.csv"));
+        csvFileReader.setLinesToSkip(1);
+
+        LineMapper<StudentDTO> studentLineMapper = createStudentLineMapper();
+        csvFileReader.setLineMapper(studentLineMapper);
+
+        return csvFileReader;
+    }
+
+    private LineMapper<StudentDTO> createStudentLineMapper() {
+        DefaultLineMapper<StudentDTO> studentLineMapper = new DefaultLineMapper<>();
+
+        LineTokenizer studentLineTokenizer = createStudentLineTokenizer();
+        studentLineMapper.setLineTokenizer(studentLineTokenizer);
+
+        FieldSetMapper<StudentDTO> studentInformationMapper = createStudentInformationMapper();
+        studentLineMapper.setFieldSetMapper(studentInformationMapper);
+
+        return studentLineMapper;
+    }
+
+    private LineTokenizer createStudentLineTokenizer() {
+        DelimitedLineTokenizer studentLineTokenizer = new DelimitedLineTokenizer();
+        studentLineTokenizer.setDelimiter(";");
+        studentLineTokenizer.setNames(new String[]{"name", "emailAddress", "purchasedPackage"});
+        return studentLineTokenizer;
+    }
+
+    private FieldSetMapper<StudentDTO> createStudentInformationMapper() {
+        BeanWrapperFieldSetMapper<StudentDTO> studentInformationMapper = new BeanWrapperFieldSetMapper<>();
+        studentInformationMapper.setTargetType(StudentDTO.class);
+        return studentInformationMapper;
+    }
+
+    @Bean
+    public ItemWriter<StudentDTO> itemWriter() {
+        return new LoggingItemWriter();
+    }
+
+    /**
+     * Creates a bean that represents the only step of our batch job.
+     * @param reader
+     * @param writer
+     * @param stepBuilderFactory
+     * @return
+     */
+    @Bean
+    public Step exampleJobStep(ItemReader<StudentDTO> reader,
+                               ItemWriter<StudentDTO> writer,
+                               StepBuilderFactory stepBuilderFactory) {
+        return stepBuilderFactory.get("exampleJobStep")
+                .<StudentDTO, StudentDTO>chunk(1)
+                .reader(reader)
+                .writer(writer)
+                .build();
+    }
+
+    /**
+     * Creates a bean that represents our example batch job.
+     * @param exampleJobStep
+     * @param jobBuilderFactory
+     * @return
+     */
+    @Bean
+    public Job exampleJob(Step exampleJobStep,
+                          JobBuilderFactory jobBuilderFactory) {
+        return jobBuilderFactory.get("exampleJob")
+                .incrementer(new RunIdIncrementer())
+                .flow(exampleJobStep)
+                .end()
+                .build();
+    }
+}
